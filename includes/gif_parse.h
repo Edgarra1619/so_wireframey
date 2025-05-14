50290fd189e4894ab7b361c6dfa0c8552a08d0b3
Edgar Thinkpad <edgarr.amorim@gmail.com>
2025-05-14 15:22:55 +0100

    parse through TREEEEES

--- includes/gif_parse.h
+++ includes/gif_parse.h
@@ -12,4 +12,24 @@ typedef struct	s_gif_header
 	char	_ignored[2];
 }	t_gif_header;
 
+
+//the branches should be a list, when using this as a general case
+//however, this is for the gif
+typedef struct	s_giftree
+{
+	struct s_giftree	*branches[255];
+	int					content;
+}	t_giftree;
+
+t_giftree	*start_tree(int ct_size)
+{
+	
+
+
+
+
+
+
+}
+
 #endif
--- srcs/gifparse.c
+++ srcs/gifparse.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	parse_color_table(t_color table[256], int size, int fd)
+void	parse_color_table(t_color *table, int size, int fd)
 {
 	int	i;
 
@@ -16,6 +16,15 @@ void	parse_color_table(t_color table[256], int size, int fd)
 		read(fd, table + i, 3);
 		i++;
 	}
+}
+
+void	parse_image(t_color *table, int size, int fd)
+{
+	t_giftree	
+
+
+
+
 }
 
 t_gif_header	parse_header(int fd)
@@ -28,13 +37,13 @@ t_gif_header	parse_header(int fd)
 
 //TODO parse 87a
 //TODO parse 89a = parse 87a but skip the extensions
-t_map	*parse_gif(const char *path)
+t_map	*parse_gif(const char *path, int *image_count)
 {
 	const int	fd = open(path, O_RDONLY);
 	t_gif_header	info;
-	t_map		*map;
-	t_color		color_table[256];
+	t_color		g_coltable[256];
 
+	*image_count = 0;
 	if (fd < 0)
 		return (NULL);
 	info = parse_header(fd);
@@ -44,13 +53,8 @@ t_map	*parse_gif(const char *path)
 		close(fd);
 		return (NULL);
 	}
-	
-	map = calloc(sizeof(t_map), 1);
-	if (!map)
-	{
-		close(fd);
-		return (NULL);
-	}
+	if (info.packed & 0b1000000)
+		parse_color_table(g_coltable, 1 << ((info.packed & 0b0111) + 1), fd);
 	close (fd);
 	return (map);
 }
